50335dcbfe0213dca28eab1240298557caa1204d
Giabao249 <nguyenaugiabao24905@gmail.com>
2026-03-21 19:23:12 +0700

    feat/ui-product: add detail product and update home carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
+      <ScrollToTop />
       <AppRoutes />
       <Toaster
         position="top-right"
