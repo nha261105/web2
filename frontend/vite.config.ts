468e736ea24df6d7f56a4bdf260f52f890d85fa2
cdanh <danhcong6205@gmail.com>
2026-04-04 10:44:09 +0700

    fix docker backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,25 @@ import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react"
 import path from "path"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),react()
+    tailwindcss(),
+    react()
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    strictPort: true,
   }
 })
